f2a8b7ad0156669e08f36332f38503584200e95b
Junsred <nicovignale0@gmail.com>
2024-12-05 16:25:26 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -104,9 +104,6 @@ const Home = () => {
           </div>
         </section>
       </div>
-    </>
-  );
-};
 
         {/* Acerca de Nosotros */}
         <section id="about" className="py-20 bg-gray-800" data-aos="fade-up">
