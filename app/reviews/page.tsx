236094b0959c81f2fa5e85badb7ef09297855f6a
Junsred <nicovignale0@gmail.com>
2025-01-11 16:38:48 -0300

    Update page.tsx

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import GlowingButton from './components/GlowingButton';
+import GlowingButton from '../GlowingButton';
 
 const Reviews = () => {
   const [reviews, setReviews] = useState([
