3ee7da4ef3e47ee77c3c418a12d8df6c49633a69
Junsred <nicovignale0@gmail.com>
2025-01-16 16:41:04 -0300

    Update Button.tsx

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,21 +1,21 @@
-// src/components/Button.tsx
+// app/components/Button.tsx
 import React from 'react';
 
 interface ButtonProps {
-  children: React.ReactNode;
-  className?: string;
-  onClick?: () => void;
+  children: React.ReactNode; // Aceptar el contenido dentro del botón
+  className?: string; // Opcional, clases adicionales para estilos
+  onClick?: () => void; // Acción opcional para manejar clics
 }
 
 const Button: React.FC<ButtonProps> = ({ children, className, onClick }) => {
   return (
-    <button
-      onClick={onClick}
-      className={`px-6 py-3 rounded-full text-white ${className}`}
+    <button 
+      onClick={onClick} 
+      className={`px-6 py-3 rounded-full text-white ${className}`} // Aplica las clases adicionales
     >
-      {children}
+      {children} {/* Contenido dentro del botón */}
     </button>
   );
 };
 
-export default Button;
+export default Button; // Exportación por defecto
