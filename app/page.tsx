a09f1d950ea33bf2388df4116792825623eb2f1f
Junsred <nicovignale0@gmail.com>
2025-01-16 17:43:51 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,10 @@ import Snowfall from 'react-snowfall';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useInView } from 'react-intersection-observer';
-import { VIPBenefits } from '../components/VIPBenefits';
-import { Card, CardContent, CardFooter, CardHeader } from "./components/Card";
-import { Button } from "./components/Button";
-import { Badge } from "./components/Badge";
+import { VIPBenefits } from './VIPBenefits';
+import { Card, CardContent, CardFooter, CardHeader } from "./ui/Card";
+import { Button } from "./ui/Button";
+import { Badge } from "./ui/Badge";
 
 const GlowingButton: React.FC<{ href: string; className?: string; children: React.ReactNode }> = ({ href, className, children }) => (
   <Link href={href} passHref>
