e973f1f2f4fbdcc2f5af842fb268486efb099e56
Junsred <nicovignale0@gmail.com>
2025-01-16 16:41:20 -0300

    Update Card.tsx

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,23 +1,13 @@
-// components/Card.tsx
-
+// app/components/Card.tsx
 import React from 'react';
 
-export const Card: React.FC<{ className?: string }> = ({ className, children }) => (
-  <div className={`bg-gray-800 rounded-lg shadow-lg ${className}`}>{children}</div>
-);
+interface CardProps {
+  className?: string; // Opcional, clases adicionales para el contenedor
+  children: React.ReactNode; // Aceptar contenido dentro del card
+}
 
-export const CardHeader: React.FC<{ className?: string }> = ({ className, children }) => (
-  <div className={`p-4 border-b border-gray-700 ${className}`}>{children}</div>
-);
-
-export const CardTitle: React.FC<{ className?: string }> = ({ className, children }) => (
-  <h3 className={`text-xl font-semibold text-white ${className}`}>{children}</h3>
-);
-
-export const CardContent: React.FC<{ className?: string }> = ({ className, children }) => (
-  <div className={`p-4 ${className}`}>{children}</div>
-);
-
-export const CardFooter: React.FC<{ className?: string }> = ({ className, children }) => (
-  <div className={`p-4 border-t border-gray-700 ${className}`}>{children}</div>
+export const Card: React.FC<CardProps> = ({ className, children }) => (
+  <div className={`bg-gray-800 rounded-lg shadow-lg ${className}`}>
+    {children} {/* Renderiza el contenido dentro del card */}
+  </div>
 );
