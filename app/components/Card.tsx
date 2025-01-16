2ce019fceb03c338e3fa855ec97575df5cf1cee1
Junsred <nicovignale0@gmail.com>
2025-01-16 16:44:45 -0300

    Update Card.tsx

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,36 +1,57 @@
 // app/components/Card.tsx
 import React from 'react';
 
+interface CardProps {
+  className?: string;
+  children: React.ReactNode; // Agregar el tipo de children
+}
+
 // Componente Card que recibe children y className como propiedades
-export const Card: React.FC<{ className?: string }> = ({ className, children }) => (
+export const Card: React.FC<CardProps> = ({ className, children }) => (
   <div className={`bg-gray-800 rounded-lg shadow-lg ${className}`}>
     {children}
   </div>
 );
 
 // Componente CardHeader
-export const CardHeader: React.FC<{ className?: string }> = ({ className, children }) => (
+interface CardHeaderProps {
+  className?: string;
+  children: React.ReactNode; // Agregar el tipo de children
+}
+export const CardHeader: React.FC<CardHeaderProps> = ({ className, children }) => (
   <div className={`p-4 border-b border-gray-700 ${className}`}>
     {children}
   </div>
 );
 
 // Componente CardTitle
-export const CardTitle: React.FC<{ className?: string }> = ({ className, children }) => (
+interface CardTitleProps {
+  className?: string;
+  children: React.ReactNode; // Agregar el tipo de children
+}
+export const CardTitle: React.FC<CardTitleProps> = ({ className, children }) => (
   <h3 className={`text-2xl font-bold ${className}`}>
     {children}
   </h3>
 );
 
 // Componente CardContent
-export const CardContent: React.FC<{ className?: string }> = ({ className, children }) => (
+interface CardContentProps {
+  className?: string;
+  children: React.ReactNode; // Agregar el tipo de children
+}
+export const CardContent: React.FC<CardContentProps> = ({ className, children }) => (
   <div className={`p-4 ${className}`}>
     {children}
   </div>
 );
 
 // Componente CardFooter
-export const CardFooter: React.FC<{ className?: string }> = ({ className, children }) => (
+interface CardFooterProps {
+  className?: string;
+  children: React.ReactNode; // Agregar el tipo de children
+}
+export const CardFooter: React.FC<CardFooterProps> = ({ className, children }) => (
   <div className={`p-4 border-t border-gray-700 ${className}`}>
     {children}
   </div>
