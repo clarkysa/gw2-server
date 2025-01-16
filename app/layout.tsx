f7f4b707794134e0090d6d0077638cc95d520839
Junsred <nicovignale0@gmail.com>
2025-01-16 18:19:30 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,8 @@
-// app/layout.tsx
-import './globals.css'; // Importa los estilos globales de Tailwind
-import '../styles/globals.css';  // Importa el archivo CSS global
+export const metadata = {
+  title: "GW2 | Página Inicial",
+  description: "Bienvenido al mundo de los gatitos nwn.",
+};
 
-export default function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="es">
-      <body>{children}</body>
-    </html>
-  );
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
 }
